47e4933d2b7bb41c52a0afc0b1a63c2160f6e0c0
guiltylord <desxzawq@gmail.com>
2025-04-17 18:12:29 +0300

    +hype: add numeration for strings in debug console

--- utils/debug.js
+++ utils/debug.js
@@ -1,9 +1,11 @@
 // Функция для вывода отладочных сообщений
+var counter = 0;
 export function debugLog(message) {
     const debugDiv = document.getElementById('debugMessages');
     if (debugDiv) {
+        counter++;
         const messageElement = document.createElement('div');
-        messageElement.textContent = message;
+        messageElement.textContent = `${counter}. `+message;
         debugDiv.appendChild(messageElement);
         debugDiv.scrollTop = debugDiv.scrollHeight;
     }
